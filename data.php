4265dc4b92f74f84e24c25a49a1e5e7f5a38723a
VirtusStack <payalgupta6403@gmail.com>
2025-06-11 10:27:39 +0530

    Create data.php

---
+++ data.php
@@ -0,0 +1,265 @@
+<?php
+$data=[
+   ["name" => "Harry Potter", "role" => "Wizard", "book" => "Harry Potter and the Philosopher's Stone"],
+   ["name" => "Hermione Granger", "role" => "Witch", "book" => "Harry Potter and the Goblet of Fire"],
+   ["name" => "Sherlock Holmes", "role" => "Detective", "book" => "A Study in Scarlet"],
+   ["name" => "Elizabeth Bennet", "role" => "Heroine", "book" => "Pride and Prejudice"],
+   ["name" => "Frodo Baggins", "role" => "Ring Bearer", "book" => "The Lord of the Rings"],
+    ["name" => "Hercule Poirot", "role" => "Detective", "book" => "Murder on the Orient Express"],
+    ["name" => "Jay Gatsby", "role" => "Millionaire", "book" => "The Great Gatsby"],
+    ["name" => "Atticus Finch", "role" => "Lawyer", "book" => "To Kill a Mockingbird"],
+    ["name" => "Arya Stark", "role" => "Assassin", "book" => "A Game of Thrones"],
+    ["name" => "Bilbo Baggins", "role" => "Adventurer", "book" => "The Hobbit"],
+    ["name" => "Nova Star", "role" => "Rogue", "book" => "Dreamwalkers"],
+    ["name" => "Nico Shade", "role" => "Inventor", "book" => "Shadow City"],
+    ["name" => "Luna Blaze", "role" => "Healer", "book" => "Frostborn"],
+    ["name" => "Nova Star", "role" => "Rogue", "book" => "Legends of Orion"],
+    ["name" => "Zara Vale", "role" => "Scholar", "book" => "Shadow City"],
+    ["name" => "Aria Moon", "role" => "Assassin", "book" => "Chronicles of Aether"],
+    ["name" => "Raya Quinn", "role" => "Warrior", "book" => "Chronicles of Aether"],
+    ["name" => "Orion Skye", "role" => "Explorer", "book" => "Mystic Realms"],
+    ["name" => "Zara Vale", "role" => "Assassin", "book" => "Shadow City"],
+    ["name" => "Jett Storm", "role" => "Detective", "book" => "Legends of Orion"],
+    ["name" => "Nova Star", "role" => "Detective", "book" => "Legends of Orion"],
+    ["name" => "Nova Star", "role" => "Assassin", "book" => "Shadow City"],
+    ["name" => "Raya Quinn", "role" => "Hunter", "book" => "Mystic Realms"],
+    ["name" => "Luna Blaze", "role" => "Hunter", "book" => "Mystic Realms"],
+    ["name" => "Luna Blaze", "role" => "Detective", "book" => "Dreamwalkers"],
+    ["name" => "Zara Vale", "role" => "Mage", "book" => "The Celestial War"],
+    ["name" => "Kai Frost", "role" => "Detective", "book" => "Frostborn"],
+    ["name" => "Aria Moon", "role" => "Mage", "book" => "Shadow City"],
+    ["name" => "Aria Moon", "role" => "Detective", "book" => "Echoes of Eternity"],
+    ["name" => "Leo Drake", "role" => "Hunter", "book" => "Dreamwalkers"],
+    ["name" => "Leo Drake", "role" => "Warrior", "book" => "The Crimson Pact"],
+    ["name" => "Raya Quinn", "role" => "Assassin", "book" => "The Forgotten Code"],
+    ["name" => "Kai Frost", "role" => "Mage", "book" => "The Celestial War"],
+    ["name" => "Nova Star", "role" => "Explorer", "book" => "Chronicles of Aether"],
+    ["name" => "Zara Vale", "role" => "Hunter", "book" => "The Crimson Pact"],
+    ["name" => "Nova Star", "role" => "Rogue", "book" => "Legends of Orion"],
+    ["name" => "Raya Quinn", "role" => "Rogue", "book" => "The Celestial War"],
+    ["name" => "Nova Star", "role" => "Warrior", "book" => "Echoes of Eternity"],
+    ["name" => "Orion Skye", "role" => "Rogue", "book" => "Dreamwalkers"],
+    ["name" => "Leo Drake", "role" => "Mage", "book" => "Mystic Realms"],
+    ["name" => "Nova Star", "role" => "Mage", "book" => "The Forgotten Code"],
+    ["name" => "Kai Frost", "role" => "Inventor", "book" => "Legends of Orion"],
+    ["name" => "Raya Quinn", "role" => "Explorer", "book" => "Chronicles of Aether"],
+    ["name" => "Kai Frost", "role" => "Explorer", "book" => "Shadow City"],
+    ["name" => "Leo Drake", "role" => "Rogue", "book" => "Mystic Realms"],
+    ["name" => "Nova Star", "role" => "Assassin", "book" => "The Forgotten Code"],
+    ["name" => "Jett Storm", "role" => "Mage", "book" => "Chronicles of Aether"],
+    ["name" => "Luna Blaze", "role" => "Hunter", "book" => "The Forgotten Code"],
+    ["name" => "Raya Quinn", "role" => "Assassin", "book" => "The Forgotten Code"],
+    ["name" => "Jett Storm", "role" => "Inventor", "book" => "The Crimson Pact"],
+    ["name" => "Luna Blaze", "role" => "Detective", "book" => "Frostborn"],
+    ["name" => "Raya Quinn", "role" => "Assassin", "book" => "Shadow City"],
+    ["name" => "Leo Drake", "role" => "Detective", "book" => "The Crimson Pact"],
+    ["name" => "Zara Vale", "role" => "Warrior", "book" => "Legends of Orion"],
+    ["name" => "Raya Quinn", "role" => "Detective", "book" => "The Celestial War"],
+    ["name" => "Nico Shade", "role" => "Detective", "book" => "Mystic Realms"],
+    ["name" => "Orion Skye", "role" => "Hunter", "book" => "Dreamwalkers"],
+    ["name" => "Luna Blaze", "role" => "Inventor", "book" => "Dreamwalkers"],
+    ["name" => "Luna Blaze", "role" => "Explorer", "book" => "Chronicles of Aether"],
+    ["name" => "Leo Drake", "role" => "Scholar", "book" => "Mystic Realms"],
+    ["name" => "Zara Vale", "role" => "Rogue", "book" => "The Celestial War"],
+    ["name" => "Zara Vale", "role" => "Explorer", "book" => "Echoes of Eternity"],
+    ["name" => "Luna Blaze", "role" => "Detective", "book" => "Echoes of Eternity"],
+    ["name" => "Nova Star", "role" => "Rogue", "book" => "Dreamwalkers"],
+    ["name" => "Luna Blaze", "role" => "Detective", "book" => "Dreamwalkers"],
+    ["name" => "Aria Moon", "role" => "Explorer", "book" => "The Crimson Pact"],
+    ["name" => "Zara Vale", "role" => "Explorer", "book" => "Shadow City"],
+    ["name" => "Orion Skye", "role" => "Mage", "book" => "The Crimson Pact"],
+    ["name" => "Luna Blaze", "role" => "Warrior", "book" => "Chronicles of Aether"],
+    ["name" => "Aria Moon", "role" => "Scholar", "book" => "Chronicles of Aether"],
+    ["name" => "Luna Blaze", "role" => "Mage", "book" => "The Crimson Pact"],
+    ["name" => "Leo Drake", "role" => "Rogue", "book" => "Frostborn"],
+    ["name" => "Kai Frost", "role" => "Hunter", "book" => "Echoes of Eternity"],
+    ["name" => "Kai Frost", "role" => "Hunter", "book" => "Echoes of Eternity"],
+    ["name" => "Orion Skye", "role" => "Scholar", "book" => "The Celestial War"],
+    ["name" => "Raya Quinn", "role" => "Inventor", "book" => "The Forgotten Code"],
+    ["name" => "Aria Moon", "role" => "Warrior", "book" => "The Forgotten Code"],
+    ["name" => "Jett Storm", "role" => "Warrior", "book" => "Shadow City"],
+    ["name" => "Leo Drake", "role" => "Rogue", "book" => "The Forgotten Code"],
+    ["name" => "Zara Vale", "role" => "Assassin", "book" => "The Crimson Pact"],
+    ["name" => "Kai Frost", "role" => "Mage", "book" => "Shadow City"],
+    ["name" => "Raya Quinn", "role" => "Mage", "book" => "The Forgotten Code"],
+    ["name" => "Jett Storm", "role" => "Rogue", "book" => "Shadow City"],
+    ["name" => "Orion Skye", "role" => "Healer", "book" => "Legends of Orion"],
+    ["name" => "Nova Star", "role" => "Hunter", "book" => "The Crimson Pact"],
+    ["name" => "Luna Blaze", "role" => "Detective", "book" => "The Crimson Pact"],
+    ["name" => "Raya Quinn", "role" => "Healer", "book" => "Legends of Orion"],
+    ["name" => "Orion Skye", "role" => "Explorer", "book" => "The Crimson Pact"],
+    ["name" => "Leo Drake", "role" => "Hunter", "book" => "The Celestial War"],
+    ["name" => "Zara Vale", "role" => "Healer", "book" => "Legends of Orion"],
+    ["name" => "Nova Star", "role" => "Mage", "book" => "The Celestial War"],
+    ["name" => "Kai Frost", "role" => "Hunter", "book" => "Shadow City"],
+    ["name" => "Nova Star", "role" => "Hunter", "book" => "Mystic Realms"],
+    ["name" => "Nico Shade", "role" => "Rogue", "book" => "Dreamwalkers"],
+    ["name" => "Jett Storm", "role" => "Rogue", "book" => "Echoes of Eternity"],
+    ["name" => "Orion Skye", "role" => "Hunter", "book" => "Echoes of Eternity"],
+    ["name" => "Nova Star", "role" => "Healer", "book" => "Frostborn"],
+    ["name" => "Nova Star", "role" => "Assassin", "book" => "Dreamwalkers"],
+    ["name" => "Raya Quinn", "role" => "Healer", "book" => "The Crimson Pact"],
+    ["name" => "Zara Vale", "role" => "Warrior", "book" => "Echoes of Eternity"],
+    ["name" => "Zara Vale", "role" => "Healer", "book" => "Chronicles of Aether"],
+    ["name" => "Leo Drake", "role" => "Mage", "book" => "Mystic Realms"],
+    ["name" => "Kai Frost", "role" => "Mage", "book" => "The Forgotten Code"],
+    ["name" => "Luna Blaze", "role" => "Assassin", "book" => "The Crimson Pact"],
+    ["name" => "Kai Frost", "role" => "Inventor", "book" => "Dreamwalkers"],
+    ["name" => "Aria Moon", "role" => "Mage", "book" => "Dreamwalkers"],
+    ["name" => "Nova Star", "role" => "Detective", "book" => "The Forgotten Code"],
+    ["name" => "Leo Drake", "role" => "Healer", "book" => "The Crimson Pact"],
+    ["name" => "Raya Quinn", "role" => "Assassin", "book" => "Frostborn"],
+    ["name" => "Jett Storm", "role" => "Scholar", "book" => "The Forgotten Code"],
+    ["name" => "Jett Storm", "role" => "Warrior", "book" => "Frostborn"],
+    ["name" => "Leo Drake", "role" => "Explorer", "book" => "Shadow City"],
+    ["name" => "Zara Vale", "role" => "Scholar", "book" => "Chronicles of Aether"],
+    ["name" => "Orion Skye", "role" => "Inventor", "book" => "Mystic Realms"],
+    ["name" => "Nova Star", "role" => "Hunter", "book" => "Dreamwalkers"],
+    ["name" => "Nova Star", "role" => "Rogue", "book" => "Frostborn"],
+    ["name" => "Orion Skye", "role" => "Explorer", "book" => "The Celestial War"],
+    ["name" => "Raya Quinn", "role" => "Mage", "book" => "Chronicles of Aether"],
+    ["name" => "Raya Quinn", "role" => "Warrior", "book" => "Legends of Orion"],
+    ["name" => "Raya Quinn", "role" => "Detective", "book" => "Dreamwalkers"],
+    ["name" => "Kai Frost", "role" => "Healer", "book" => "Dreamwalkers"],
+    ["name" => "Orion Skye", "role" => "Healer", "book" => "The Forgotten Code"],
+    ["name" => "Zara Vale", "role" => "Explorer", "book" => "The Forgotten Code"],
+    ["name" => "Nico Shade", "role" => "Inventor", "book" => "The Crimson Pact"],
+    ["name" => "Aria Moon", "role" => "Rogue", "book" => "Chronicles of Aether"],
+    ["name" => "Nico Shade", "role" => "Hunter", "book" => "The Celestial War"],
+    ["name" => "Jett Storm", "role" => "Healer", "book" => "Legends of Orion"],
+    ["name" => "Nico Shade", "role" => "Inventor", "book" => "Dreamwalkers"],
+    ["name" => "Aria Moon", "role" => "Mage", "book" => "The Celestial War"],
+    ["name" => "Luna Blaze", "role" => "Assassin", "book" => "The Celestial War"],
+    ["name" => "Jett Storm", "role" => "Explorer", "book" => "Legends of Orion"],
+    ["name" => "Aria Moon", "role" => "Mage", "book" => "Chronicles of Aether"],
+    ["name" => "Aria Moon", "role" => "Inventor", "book" => "The Crimson Pact"],
+    ["name" => "Luna Blaze", "role" => "Healer", "book" => "Frostborn"],
+    ["name" => "Zara Vale", "role" => "Mage", "book" => "The Crimson Pact"],
+    ["name" => "Leo Drake", "role" => "Warrior", "book" => "Chronicles of Aether"],
+    ["name" => "Jett Storm", "role" => "Detective", "book" => "The Celestial War"],
+    ["name" => "Aria Moon", "role" => "Scholar", "book" => "Chronicles of Aether"],
+    ["name" => "Kai Frost", "role" => "Mage", "book" => "Chronicles of Aether"],
+    ["name" => "Nova Star", "role" => "Detective", "book" => "Legends of Orion"],
+    ["name" => "Jett Storm", "role" => "Explorer", "book" => "Echoes of Eternity"],
+    ["name" => "Nova Star", "role" => "Scholar", "book" => "Chronicles of Aether"],
+    ["name" => "Zara Vale", "role" => "Warrior", "book" => "Chronicles of Aether"],
+    ["name" => "Leo Drake", "role" => "Scholar", "book" => "Echoes of Eternity"],
+    ["name" => "Jett Storm", "role" => "Mage", "book" => "Frostborn"],
+    ["name" => "Nova Star", "role" => "Scholar", "book" => "Frostborn"],
+    ["name" => "Luna Blaze", "role" => "Warrior", "book" => "Mystic Realms"],
+    ["name" => "Raya Quinn", "role" => "Mage", "book" => "Mystic Realms"],
+    ["name" => "Zara Vale", "role" => "Assassin", "book" => "Frostborn"],
+    ["name" => "Zara Vale", "role" => "Rogue", "book" => "The Crimson Pact"],
+    ["name" => "Aria Moon", "role" => "Healer", "book" => "Echoes of Eternity"],
+    ["name" => "Kai Frost", "role" => "Warrior", "book" => "The Crimson Pact"],
+    ["name" => "Jett Storm", "role" => "Explorer", "book" => "The Forgotten Code"],
+    ["name" => "Kai Frost", "role" => "Assassin", "book" => "The Crimson Pact"],
+    ["name" => "Aria Moon", "role" => "Healer", "book" => "Shadow City"],
+    ["name" => "Leo Drake", "role" => "Mage", "book" => "Shadow City"],
+    ["name" => "Orion Skye", "role" => "Explorer", "book" => "Shadow City"],
+    ["name" => "Nico Shade", "role" => "Healer", "book" => "Echoes of Eternity"],
+    ["name" => "Nova Star", "role" => "Healer", "book" => "The Crimson Pact"],
+    ["name" => "Raya Quinn", "role" => "Explorer", "book" => "The Crimson Pact"],
+    ["name" => "Luna Blaze", "role" => "Healer", "book" => "The Forgotten Code"],
+    ["name" => "Leo Drake", "role" => "Hunter", "book" => "The Crimson Pact"],
+    ["name" => "Kai Frost", "role" => "Warrior", "book" => "Legends of Orion"],
+    ["name" => "Nico Shade", "role" => "Healer", "book" => "Frostborn"],
+    ["name" => "Zara Vale", "role" => "Hunter", "book" => "Frostborn"],
+    ["name" => "Leo Drake", "role" => "Warrior", "book" => "The Celestial War"],
+    ["name" => "Aria Moon", "role" => "Healer", "book" => "Dreamwalkers"],
+    ["name" => "Kai Frost", "role" => "Detective", "book" => "The Forgotten Code"],
+    ["name" => "Jett Storm", "role" => "Warrior", "book" => "The Celestial War"],
+    ["name" => "Jett Storm", "role" => "Warrior", "book" => "Legends of Orion"],
+    ["name" => "Raya Quinn", "role" => "Explorer", "book" => "Legends of Orion"],
+    ["name" => "Nova Star", "role" => "Healer", "book" => "The Crimson Pact"],
+    ["name" => "Nova Star", "role" => "Assassin", "book" => "The Celestial War"],
+    ["name" => "Raya Quinn", "role" => "Explorer", "book" => "Dreamwalkers"],
+    ["name" => "Leo Drake", "role" => "Healer", "book" => "Mystic Realms"],
+    ["name" => "Aria Moon", "role" => "Detective", "book" => "The Crimson Pact"],
+    ["name" => "Aria Moon", "role" => "Hunter", "book" => "The Forgotten Code"],
+    ["name" => "Nico Shade", "role" => "Inventor", "book" => "The Forgotten Code"],
+    ["name" => "Aria Moon", "role" => "Hunter", "book" => "Echoes of Eternity"],
+    ["name" => "Kai Frost", "role" => "Explorer", "book" => "Shadow City"],
+    ["name" => "Jett Storm", "role" => "Mage", "book" => "Dreamwalkers"],
+    ["name" => "Zara Vale", "role" => "Assassin", "book" => "Echoes of Eternity"],
+    ["name" => "Leo Drake", "role" => "Scholar", "book" => "Dreamwalkers"],
+    ["name" => "Aria Moon", "role" => "Rogue", "book" => "Shadow City"],
+    ["name" => "Raya Quinn", "role" => "Explorer", "book" => "The Crimson Pact"],
+    ["name" => "Nova Star", "role" => "Scholar", "book" => "Mystic Realms"],
+    ["name" => "Zara Vale", "role" => "Detective", "book" => "The Celestial War"],
+    ["name" => "Luna Blaze", "role" => "Rogue", "book" => "The Crimson Pact"],
+    ["name" => "Nova Star", "role" => "Scholar", "book" => "The Crimson Pact"],
+    ["name" => "Raya Quinn", "role" => "Warrior", "book" => "The Crimson Pact"],
+    ["name" => "Aria Moon", "role" => "Mage", "book" => "Frostborn"],
+    ["name" => "Leo Drake", "role" => "Healer", "book" => "Chronicles of Aether"],
+    ["name" => "Kai Frost", "role" => "Mage", "book" => "Mystic Realms"],
+    ["name" => "Aria Moon", "role" => "Detective", "book" => "Chronicles of Aether"],
+    ["name" => "Zara Vale", "role" => "Healer", "book" => "Echoes of Eternity"],
+    ["name" => "Orion Skye", "role" => "Healer", "book" => "Frostborn"],
+    ["name" => "Jett Storm", "role" => "Rogue", "book" => "The Celestial War"],
+    ["name" => "Nova Star", "role" => "Assassin", "book" => "Dreamwalkers"],
+    ["name" => "Raya Quinn", "role" => "Detective", "book" => "Mystic Realms"],
+    ["name" => "Leo Drake", "role" => "Mage", "book" => "Shadow City"],
+    ["name" => "Nico Shade", "role" => "Rogue", "book" => "The Celestial War"],
+    ["name" => "Nico Shade", "role" => "Detective", "book" => "The Forgotten Code"],
+    ["name" => "Aria Moon", "role" => "Healer", "book" => "Shadow City"],
+    ["name" => "Orion Skye", "role" => "Healer", "book" => "Dreamwalkers"],
+    ["name" => "Kai Frost", "role" => "Detective", "book" => "The Celestial War"],
+    ["name" => "Nico Shade", "role" => "Warrior", "book" => "Dreamwalkers"],
+    ["name" => "Jett Storm", "role" => "Explorer", "book" => "Dreamwalkers"],
+    ["name" => "Nico Shade", "role" => "Hunter", "book" => "Dreamwalkers"],
+    ["name" => "Orion Skye", "role" => "Detective", "book" => "The Forgotten Code"],
+    ["name" => "Orion Skye", "role" => "Mage", "book" => "The Forgotten Code"],
+    ];
+    ?>
+<!DOCTYPE html>
+<html lang = "en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewpoint" content="width=device-width, initial-scale="1.0">
+<title>Book Characters</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
+</head>
+<body class="bg-light">
+<div class="container py-5">
+<h2 class="text-center mb-4">Book Characters</h2>
+<div class="table-responsive">
+<table id="myTable" class="table table-striped table-bordered">
+<thead class="table-dark">
+<tr>
+<th>Name</th>
+<th>Roles</th>
+<th>Book</th>
+</tr>
+</thead>
+<tbody>
+<?php foreach($data as $row): ?>
+<tr>
+<td><?= htmlspecialchars($row['name']) ?></td>
+<td><?= htmlspecialchars($row['role']) ?></td>
+<td><?= htmlspecialchars($row['book']) ?></td>
+</tr>
+<?php endforeach; ?>
+</tbody>
+</table>
+</div>
+</div>
+ <script>
+ $(document).ready(function() {
+    $('#myTable').DataTable({
+     "lengthMenu":[5, 10, 25, 50, 100],
+    "pagingType": "simple_numbers",
+    "language": {
+    "search" : "Filter records:"
+   }
+});
+});
+  </script>	
+ 
+</body>
+</html>
